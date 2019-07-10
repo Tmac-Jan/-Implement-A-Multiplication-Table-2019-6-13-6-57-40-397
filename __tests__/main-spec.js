6d591bc7ad5365dce97d9111345e3ff7a63a2a0e
zhanggi3 <gio.zhang@oocl.com>
2019-07-10 08:26:10 +0800

    update the module

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -28,13 +28,14 @@ it ('should generateMultiplicationTable  when give two valid multipliers then ge
       const reString='2*1=2 2*2=4 \n3*1=3 3*2=6 3*3=9 \n4*1=4 4*2=8 4*3=12 4*4=16 '
         expect(generateMultiplicationTable(2, 4)).toBe(reString);
 });
-it ('should get a getMultiplicationTable String when give two multipliers  ', () => {
+it ('should get a generateMultiplicationTable String when give two multipliers  ', () => {
     const reString='2*1=2 2*2=4 \n'+'3*1=3 3*2=6 3*3=9 \n'+'4*1=4 4*2=8 4*3=12 4*4=16 \n';
     expect(generateMultiplicationTable(2, 4)).toBe(reString);
 });
-it ('should get a getMultiplicationTable String when give one valid multipliers and a not valid multipliers  ', () => {
+it ('should get a generateMultiplicationTable String when give two multipliers  ', () => {
+    const reString='2*1=2 2*2=4 \n'+'3*1=3 3*2=6 3*3=9 \n'+'4*1=4 4*2=8 4*3=12 4*4=16 \n';
+    expect(getMultiplicationTable(2, 4)).toBe(reString);
+});
+it ('should get a generateMultiplicationTable String when give one valid multipliers and a not valid multipliers  ', () => {
     expect(generateMultiplicationTable(0, 2)).toBe(null);
 });
-//it ('should get a String when give two multipliers  ', () => {
-//    expect(getEquationString(1, 2)).toBe('1*2=2 ');
-//});
